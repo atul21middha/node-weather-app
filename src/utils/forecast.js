fba0b08046af2ebc820c956df22d15c2e2698852
Atul Midha <atul.midda@g-axon.com>
2020-05-08 13:02:28 +0530

    updated key

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.weatherstack.com/current?access_key=93b63192256d48b43f8d41b0f54cba85/' + latitude + ',' + longitude
+    const url = 'http://api.weatherstack.com/current?access_key=93b63192256d48b43f8d41b0f54cba85&query=' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
         console.log("body", body);
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
         }
     })
 }
